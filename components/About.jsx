1f338ae497e8428f6fb0f4c229efbedb81d8425b
MDeFaria <maria.defariasf@gmail.com>
2024-09-24 18:39:56 -0400

    [add] componente about agregado al flujo

---
+++ components/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <section className="section">
+      <div className="w-full justify-center text-center">
+        <h2 className="h2">ABOUT OUR APP</h2>
+        <p className="mx-auto max-w-[600px]">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+          magni, sunt qui earum nam distinctio illo sapiente vitae ea, deleniti
+          beatae nulla ratione obcaecati omnis accusantium fugit corporis
+          inventore recusandae.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  );
+};
